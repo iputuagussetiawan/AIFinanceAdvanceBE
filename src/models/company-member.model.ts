2d3473578c9c812d11adbf3cfd9ff081dd6e46b5
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2026-02-17 14:52:58 +0800

    create model

--- src/models/company-member.model.ts
+++ src/models/company-member.model.ts
@@ -1,16 +1,19 @@
 import mongoose, { Document, Schema } from "mongoose";
+import type { RoleDocument } from "./roles-permission.model";
 
-export interface CompanyUserDocument extends Document {
+export interface CompanyMemberDocument extends Document {
     isActive: boolean;
     // --- ADDED RELATIONSHIP ---
     companyId: mongoose.Types.ObjectId | null; 
     userId: mongoose.Types.ObjectId | null; 
+    role: RoleDocument;
+    joinedAt: Date;
     // ----END RELATIONSHIP ----
     createdAt: Date;
     updatedAt: Date;
 }
 
-const companyUserSchema = new Schema<CompanyUserDocument>(
+const companyMemberSchema = new Schema<CompanyMemberDocument>(
     {
         isActive: { type: Boolean, default: true },
         // --- ADDED RELATIONSHIP FIELD ---
@@ -20,11 +23,18 @@ const companyUserSchema = new Schema<CompanyUserDocument>(
             default: null,
             index: true, // Recommended for faster queries when filtering by company
         },
+
         userId: {
             type: Schema.Types.ObjectId,
             ref: "User", // This must match the name used in your User model
             default: null,
             index: true, // Recommended for faster queries when filtering by company
+        },
+
+        role: {
+            type: Schema.Types.ObjectId,
+            ref: "Role",
+            required: true,
         },
         // ---END ADDED RELATIONSHIP FIELD ---
     },
@@ -34,5 +44,5 @@ const companyUserSchema = new Schema<CompanyUserDocument>(
 );
 
 //This code is the final step in setting up your database model. It takes your configuration (the Schema) and creates a functional tool (the Model) that allows you to interact with the MongoDB database.
-const CompanyUserModel = mongoose.model<CompanyUserDocument>("CompanyUser", companyUserSchema);
+const CompanyUserModel = mongoose.model<CompanyMemberDocument>("CompanyMember", companyMemberSchema);
 export default CompanyUserModel;
