a72c39a3bf43b3a97fa40e806c2eb8c7a3c959c1
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2026-03-04 20:24:14 +0800

    remove asset after delete

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,6 +1,7 @@
 import { BadRequestException, NotFoundException } from '../../utils/appError'
 import UserModel from './user.model'
 import type { UpdateUserInputType } from './user.validation'
+import { v2 as cloudinary } from 'cloudinary'
 
 export const getCurrentUserService = async (userId: string) => {
     const user = await UserModel.findById(userId)
@@ -23,11 +24,28 @@ export const updateUserService = async (
     if (!user) throw new NotFoundException('User not found')
 
     if (profilePic) {
+        if (user.profilePicture && user.profilePicture.includes('cloudinary')) {
+            try {
+                // Robust extraction: get everything between /upload/v12345/ and the extension
+                const splitUrl = user.profilePicture.split('/')
+                const lastPart = splitUrl[splitUrl.length - 1].split('.')[0]
+                const folderPart = splitUrl[splitUrl.length - 2]
+
+                // If your images are in a folder, publicId must be "folder/filename"
+                const publicId = user.profilePicture.includes('upload/v')
+                    ? `${folderPart}/${lastPart}`
+                    : lastPart
+
+                await cloudinary.uploader.destroy(publicId, { invalidate: true })
+            } catch (error) {
+                console.error('Cloudinary cleanup failed:', error)
+            }
+        }
         user.profilePicture = profilePic.path
     }
-    user.set({
-        name: body.name
-    })
+
+    user.name = body.name || user.name
+
     await user.save()
     return user.omitPassword()
 }
